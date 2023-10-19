8312e1cd9ef8f57d360103c99ce17f853b4dc133
Syafa Ramadhan <144409844+Syauqi7@users.noreply.github.com>
2023-10-19 14:37:25 +0700

    Update users.php

--- users.php
+++ users.php
@@ -217,7 +217,7 @@
                     </ul>
                 </nav>
                 <p class="copyright pull-right">
-                    &copy; <script>document.write(new Date().getFullYear())</script>
+                    &copy; <script>document.write(new Date().getFullYear())</script> <a href="https://smk.prestasiprima.sch.id/">SMK Prestasi Prima</a>, Jl. Assyafiah, Kec. Cipayung, Jakarta Timur.
                 </p>
             </div>
         </footer>
